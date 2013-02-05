e90f5cc16dbf1847e36d793aae7f1b24e0f8faf0
aurelw <aurel.w@gmail.com>
2013-02-05 17:59:10 +0100

    Test Module. Tests for RigConfig.

---
+++ src/tests/test_rig_config.cpp
@@ -0,0 +1,29 @@
+/*
+   * (C) 2013, Aurel Wildfellner
+   *
+   * This file is part of Beholder.
+   *
+   * Beholder is free software: you can redistribute it and/or modify
+   * it under the terms of the GNU General Public License as published by
+   * the Free Software Foundation, either version 3 of the License, or
+   * (at your option) any later version.
+   *
+   * Beholder is distributed in the hope that it will be useful,
+   * but WITHOUT ANY WARRANTY; without even the implied warranty of
+   * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   * GNU General Public License for more details.
+   *
+   * You should have received a copy of the GNU General Public License
+   * along with Beholder. If not, see <http://www.gnu.org/licenses/>. */
+
+#include "rig_config.h"
+
+int main() {
+
+    RigConfig rc;
+    rc.saveToFile("test_rig_config.xml");
+    rc.loadFromFile("test_rig_config.xml");
+    rc.saveToFile("test_rig_config_loaded.xml");
+
+}
+
