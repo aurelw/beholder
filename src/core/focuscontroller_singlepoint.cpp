51801999ffc1fe49829d930cead2e5aad3dcf16f
aurel <aurel@mrbump>
2013-02-13 18:21:41 +0100

    FocusControllerSinglePoint, first implementation of a point tracker.

---
+++ src/core/focuscontroller_singlepoint.cpp
@@ -0,0 +1,106 @@
+/*
+   * (C) 2013, Aurel Wildfellner
+   *
+   * This file is part of Beholder.
+   *
+   * Beholder is free software: you can redistribute it and/or modify
+   * it under the terms of the GNU General Public License as published by
+   * the Free Software Foundation, either version 3 of the License, or
+   * (at your option) any later version.
+   *
+   * Beholder is distributed in the hope that it will be useful,
+   * but WITHOUT ANY WARRANTY; without even the implied warranty of
+   * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   * GNU General Public License for more details.
+   *
+   * You should have received a copy of the GNU General Public License
+   * along with Beholder. If not, see <http://www.gnu.org/licenses/>. */
+
+#include "focuscontroller_singlepoint.h"
+
+
+FocusControllerSinglePoint::FocusControllerSinglePoint(
+        CameraParameters::Ptr camPar,
+        PoseTracker::Ptr pTracker)
+{
+    camParameters = camPar;
+    poseTracker = pTracker; 
+    focusMessure.setCameraParameters(camParameters);
+}
+
+
+void FocusControllerSinglePoint::start() {
+    if (!threadRunning) {
+        threadRunning = true;
+        thread = new boost::thread(
+                boost::bind( &FocusControllerSinglePoint::runThread, this ));
+    }
+}
+
+
+void FocusControllerSinglePoint::stop() {
+    if (threadRunning) {
+        /* stop the thread */
+        stopThread = true;
+        thread->join();
+        delete thread;
+        threadRunning = false;
+        stopThread = false;
+
+        /* delegate focus control */
+        activeState = false;
+    }
+}
+
+
+void FocusControllerSinglePoint::reset() {
+    boost::unique_lock<boost::shared_mutex> lock(poiMutex);
+    poi.reset();
+}
+
+
+void FocusControllerSinglePoint::addPOI(PointOfInterest::Ptr p) {
+    poi = p;
+}
+
+
+void FocusControllerSinglePoint::runThread() {
+    while (!stopThread) {
+        doTracking();
+        boost::this_thread::sleep(
+                boost::posix_time::milliseconds(threadSleepMillis));
+    }
+}
+
+
+void FocusControllerSinglePoint::doTracking() {
+
+    bool canTrack = false;
+    float distance = 1.0;
+
+    Eigen::Affine3f cameraPose = poseTracker->getPose();
+    focusMessure.setPose(cameraPose);
+
+    /* get the point to track safely */
+    pcl::PointXYZ trackedPoint;
+    bool gotPoint = false;
+    {
+        boost::shared_lock<boost::shared_mutex> lock(poiMutex);
+        if (PointOfInterest::Ptr p = poi.lock()) {
+            trackedPoint = p->getPoint();
+            gotPoint = true;
+        }
+    }
+
+    if (gotPoint) {
+        Eigen::Vector3f tp(trackedPoint.x, trackedPoint.y, trackedPoint.z);
+        if (focusMessure.isVisible(tp)) {
+            distance = focusMessure.getFocalPlaneDistance(tp);
+            canTrack = true;
+        }
+    }
+
+    // write the result
+    setFocusData(canTrack, distance, trackedPoint);
+}
+
---
+++ src/core/focuscontroller_singlepoint.h
@@ -0,0 +1,63 @@
+/*
+   * (C) 2013, Aurel Wildfellner
+   *
+   * This file is part of Beholder.
+   *
+   * Beholder is free software: you can redistribute it and/or modify
+   * it under the terms of the GNU General Public License as published by
+   * the Free Software Foundation, either version 3 of the License, or
+   * (at your option) any later version.
+   *
+   * Beholder is distributed in the hope that it will be useful,
+   * but WITHOUT ANY WARRANTY; without even the implied warranty of
+   * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   * GNU General Public License for more details.
+   *
+   * You should have received a copy of the GNU General Public License
+   * along with Beholder. If not, see <http://www.gnu.org/licenses/>. */
+
+#ifndef __FOCUS_CONTROLLER_SINGLE_POINT_H__
+#define __FOCUS_CONTROLLER_SINGLE_POINT_H__
+
+#include "focuscontroller.h"
+#include "cameraparameters.h"
+#include "posetracker.h"
+#include "ffocus_messure.h"
+
+
+class FocusControllerSinglePoint : public FocusController {
+
+    public:
+
+        FocusControllerSinglePoint(CameraParameters::Ptr camPar,
+                PoseTracker::Ptr pTracker);
+
+        /* FocusController implementation */
+        virtual void start();
+        virtual void stop();
+        virtual void reset();
+        virtual void addPOI(PointOfInterest::Ptr p);
+
+    protected:
+
+        /* implementation of actual focus tracking */
+        void doTracking();
+
+        /* threading */
+        boost::thread *thread;
+        bool threadRunning = false;
+        bool stopThread = false;
+        virtual void runThread();
+        float threadSleepMillis = 10;
+
+        /* POI storage */
+        boost::shared_mutex poiMutex;
+        PointOfInterest::WeakPtr poi;
+
+        /* helpers */
+        CameraParameters::Ptr camParameters;
+        PoseTracker::Ptr poseTracker;
+        FFocusMessure focusMessure;
+};
+
+#endif
